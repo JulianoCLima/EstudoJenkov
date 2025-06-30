c4d24141244b925e81dc13e404e6aaa3d730aafb
juliano Cavalcante <julianoclima@gmail.com>
2025-06-29 23:02:20 -0300

    up

--- EstudoJavaJenkov/src/variaveis/variavel1.java
+++ EstudoJavaJenkov/src/variaveis/variavel1.java
@@ -3,10 +3,12 @@ package variaveis;
 public class variavel1 {
 	public static void main(String[] args) {
 		
-		System.out.println("teste");
-		System.out.println("juliano");
-		System.out.println("Cavalcante");
-		System.out.println("Lima");
+		int number = 10;
+		number = number + 20;
+		
+		System.out.println(number);
+
+		
 	}
 
 }
